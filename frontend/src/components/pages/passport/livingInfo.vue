678d1a13f4e9faedec1acdb0aa9fdfcbfeaf568e
Crazyteea <lipatow.nikita@yandex.ru>
2020-06-22 22:43:47 +0300

    wip living info

--- frontend/src/components/pages/passport/livingInfo.vue
+++ frontend/src/components/pages/passport/livingInfo.vue
@@ -178,6 +178,89 @@
                     </b-table-simple>
                 </b-tab>
             </b-tabs>
+            <div class="row">
+                <div class="col-6">
+                    <label for="live_proz_poluch_step">Проживающие из числа обучающихся, получающие государственную соц. стипендию</label>
+                </div>
+                <div class="col-6">
+                    <b-input-group append="Человек">
+                        <template v-slot:prepend>
+                            <b-input-group-text >
+                                <i id="bitch_tooltip" class="fas fa-question-circle"></i>
+                            </b-input-group-text>
+                            <b-tooltip custom-class="tooltip_width" target="bitch_tooltip">
+                                Количество проживающих, указанных в ч.5 ст.36 от 29.12.2012
+                                № 273-ФЗ «Об образовании в Российской Федерации»
+                            </b-tooltip>
+                        </template>
+                        <b-form-input id="live_proz_poluch_step"/>
+                    </b-input-group>
+
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="row">
+                <div class="col-6">
+                    <label for="live_proz_person">Проживающие из числа персонала и их семей</label>
+                </div>
+                <div class="col-6">
+                    <b-input-group append="Человек">
+                        <b-form-input id="live_proz_person"/>
+                    </b-input-group>
+                </div>
+            </div>
+            <br>
+            
+            <b-table-simple borderless small>
+                <b-thead>
+                    <b-tr>
+                        <b-th>Текст</b-th>
+                        <b-th>Персонал</b-th>
+                        <b-th>Члены семей</b-th>
+                    </b-tr>
+                </b-thead>
+                <b-tbody>
+                    <b-tr>
+                        <b-td>Работники</b-td>
+                        <b-td><b-form-input/></b-td>
+                        <b-td><b-form-input/></b-td>
+                    </b-tr>
+                    <b-tr>
+                        <b-td>Научные сотрудники</b-td>
+                        <b-td><b-form-input/></b-td>
+                        <b-td><b-form-input/></b-td>
+                    </b-tr>
+                    <b-tr>
+                        <b-td>Профессорско-преподавательский состав</b-td>
+                        <b-td><b-form-input/></b-td>
+                        <b-td><b-form-input/></b-td>
+                    </b-tr>
+                    <b-tr>
+                        <b-td>Иные категории сотрудников организации</b-td>
+                        <b-td><b-form-input/></b-td>
+                        <b-td><b-form-input/></b-td>
+                    </b-tr>
+                    <b-tr>
+                        <b-td>Всего</b-td>
+                        <b-td><b-form-input disabled/></b-td>
+                        <b-td><b-form-input disabled/></b-td>
+                    </b-tr>
+                </b-tbody>
+            </b-table-simple>
+            <hr>
+
+            <div class="row">
+                <div class="col-6">
+                    <label for="live_inie_prozh">Иные проживающие</label>
+                </div>
+                <div class="col-6">
+                    <b-input-group append="Человек">
+                        <b-form-input id="live_inie_prozh"/>
+                    </b-input-group>
+                </div>
+            </div>
 
         </div>
     </div>
@@ -185,7 +268,8 @@
 
 <script>
     import NavBar from "../../organisms/NavBar";
-    import {BFormInput,BTableSimple,BThead,BTbody,BTh,BTd,BTr,
+    import {BFormInput,BTableSimple,BInputGroupText,
+        BThead,BTbody,BTh,BTd,BTr,BTooltip,
         BInputGroup,BTabs,BTab} from 'bootstrap-vue'
     export default {
         name: "livingInfo",
@@ -197,6 +281,8 @@
             BTableSimple,
             BThead,BTbody,
             BTh,BTd,BTr,
+            BInputGroupText,
+            BTooltip
         },
         data(){
             return {
