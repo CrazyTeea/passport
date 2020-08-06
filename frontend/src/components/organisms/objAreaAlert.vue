8616cdc54b57db0d5d1815274e2aec54aa8f2f58
CrazyTea <lipatow.nikita@yandex.ru>
2020-08-06 17:12:32 +0300

    fix

--- frontend/src/components/organisms/objAreaAlert.vue
+++ frontend/src/components/organisms/objAreaAlert.vue
@@ -1,11 +1,11 @@
 <template>
   <b-alert v-if="objectsToShow.length" variant="danger" :show="showAlert">
     В следующий объектах не заполнена информация по площади, вычислиямые поля могут быть не корректны
-    <transition-group tag="ul">
+    <ul>
       <li v-for="(obj,index) in objectsToShow" :key="`obj_${index}`">
         <a href="/objects-area">{{obj.name}}</a>
       </li>
-    </transition-group>
+    </ul>
   </b-alert>
 </template>
 
@@ -27,7 +27,6 @@ export default {
       if (!item.area)
         return item
     })
-    console.log(this.dirtyObjects)
   }
 }
 </script>
