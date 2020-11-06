05bb8e55cbffe0a3e0cf8f00ec648b09789b14f9
CrazyTea <lipatow.nikita@yandex.ru>
2020-11-06 12:46:12 +0300

    f

--- views/app/export/_stat.php
+++ views/app/export/_stat.php
@@ -1,14 +1,56 @@
 <?php
 
-function cntInfo($info){
+function cntLiving($items,$inos = false,$inv = false)
+{
     $cnt = 0;
-    if ($info){
-        foreach ($info as $item){
-            foreach ($item as $key => $val) {
-             if ($key == 'id' or $key=='id_org' or $key=='stud_type')
-                 continue;
-             if ($item->$key)
-                $cnt++;
+
+
+
+    foreach ($items as $item) {
+        foreach ($item as $key => $val) {
+            if ($key == 'id' or
+                $key == 'id_org' or
+                $key == 'id_living' or
+                $key == 'budjet_type' or
+                $key == 'invalid' or $key =='type')
+                continue;
+            if ($item->$key)
+                if (!$inos and !$inv and !$item->invalid and in_array($item->type,['rf_och','rf_zaoch','rf_ochzaoch']))
+                    $cnt++;
+                else if ($inos and !$inv and !$item->invalid)
+                    $cnt++;
+                else if ($item->invalid and $inv and $inos)
+                    $cnt++;
+        }
+    }
+    return $cnt;
+}
+
+function cnt($info)
+{
+    $cnt = 0;
+    if ($info) {
+        if (is_array($info))
+            foreach ($info as $item) {
+                foreach ($item as $key => $val) {
+
+                    if ($key == 'id' or
+                        $key == 'id_org' or
+                        $key == 'stud_type')
+                        continue;
+                    if ($item->$key)
+                        $cnt++;
+                }
+            }
+        else {
+            foreach ($info as $key => $val) {
+
+                if ($key == 'id' or
+                    $key == 'id_org' or
+                    $key == 'stud_type')
+                    continue;
+                if ($info->$key)
+                    $cnt++;
             }
         }
     }
@@ -25,10 +67,14 @@ function cntInfo($info){
         <td>Субъект</td>
         <td>Фоив</td>
         <td>Количество заполненных полей в части информации об организации</td>
-        <td>%  заполненных полей в части информации об организации</td>
+        <td>% заполненных полей в части информации об организации</td>
         <td>Количество внесённых стран в информации о проживающих</td>
-        <td>Количество заполненных полей в информации о проживающих за исключением проживающих иностранцев и лиц с органиченными возможностями</td>
-        <td>% заполненных полей в информации о проживающих за исключением проживающих иностранцев и лиц с органиченными возможностями</td>
+        <td>Количество заполненных полей в информации о проживающих за исключением проживающих иностранцев и лиц с
+            органиченными возможностями
+        </td>
+        <td>% заполненных полей в информации о проживающих за исключением проживающих иностранцев и лиц с органиченными
+            возможностями
+        </td>
         <td>Количество заполненных полей в информации о проживающих с ограниченными возможностями</td>
         <td>% заполненных полей в информации о проживающих с ограниченными возможностями</td>
         <td>Суммарное количество заполненных полей в части информации об общежитиях</td>
@@ -46,22 +92,22 @@ function cntInfo($info){
     </thead>
     <tbody>
     <?php foreach ($orgs as $org): ?>
-    <tr>
-        <td><?=htmlspecialchars($org->id)?></td>
-        <td><?=htmlspecialchars($org->name)?></td>
-        <td><?=htmlspecialchars($org->region->region)?></td>
-        <td><?=htmlspecialchars($org->founder->founder)?></td>
-        <td><?=cntInfo($org->info)?></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
+        <tr>
+            <td><?= htmlspecialchars($org->id) ?></td>
+            <td><?= htmlspecialchars($org->name) ?></td>
+            <td><?= htmlspecialchars($org->region->region) ?></td>
+            <td><?= htmlspecialchars($org->founder->founder) ?></td>
+            <td><?= $cnt = cnt($org->info) + cnt($org->area) ?></td>
+            <td><?= round($cnt * 100 / 72) ?>%</td>
+            <td><?= \app\models\OrgLivingStudents::find()->where(['id_org' => $org->id])->andWhere(['is not', 'name', null])->groupBy('name')->count() ?></td>
+            <td><?= $cnt = cntLiving($org->livingStudents)?></td>
+            <td><?= round($cnt * 100 / 84) ?>%</td>
+            <td><?= $cnt = cntLiving($org->livingStudents,true,true)?></td>
+            <td><?= round($cnt * 100 / 168) ?>%</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
     <?php endforeach; ?>
     </tbody>
 </table>
