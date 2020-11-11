3f85836877c7194153e4cbae18d16021e36dd425
CrazyTea <lipatow.nikita@yandex.ru>
2020-11-11 16:08:00 +0300

    fix

--- frontend/src/components/organisms/NavBar.vue
+++ frontend/src/components/organisms/NavBar.vue
@@ -1,76 +1,92 @@
 <template>
   <div>
     <div class="animate-height">
-    <div class="custom-navbar">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col m-auto">
+      <div class="custom-navbar">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col m-auto">
 
-            <b-dropdown variant="transparent" menu-class="dropdown-menu" toggle-class=" text-decoration-none" no-caret>
-              <template v-slot:button-content>
-                <i class="fas fa-align-justify"></i>
-              </template>
-              <b-dropdown-item :href="isAdmin ? `/admin/data/${id_org}` : '/main'">Главная</b-dropdown-item>
-              <b-dropdown-text>
-                <i>
-                  Данные об организации
-                </i>
-              </b-dropdown-text>
-              <b-dropdown-item :href="isAdmin ? `/admin/org-info/${id_org}` : '/org-info'">Сведения об организации</b-dropdown-item>
-              <b-dropdown-item :href="isAdmin ? `/admin/area-info/${id_org}` : '/area-info'">Сведения о колличесве мест и площади</b-dropdown-item>
-              <b-dropdown-item :href="isAdmin ? `/admin/living-info/${id_org}` : '/living-info'">Сведения о проживающих</b-dropdown-item>
-              <b-dropdown-item :href="isAdmin ? `/admin/living-info-inv/${id_org}` : '/living-info-inv'">Сведения о проживающих лицах <br> с ограниченными возможностями
-              </b-dropdown-item>
-              <b-dropdown-text>
+              <b-dropdown variant="transparent" menu-class="dropdown-menu" toggle-class=" text-decoration-none"
+                          no-caret>
+                <template v-slot:button-content>
+                  <i class="fas fa-align-justify"></i>
+                </template>
+                <b-dropdown-item :href="isAdmin ? `/admin/data/${id_org}` : '/main'">Главная</b-dropdown-item>
+                <b-dropdown-text>
+                  <i>
+                    Данные об организации
+                  </i>
+                </b-dropdown-text>
+                <b-dropdown-item :href="isAdmin ? `/admin/org-info/${id_org}` : '/org-info'">Сведения об организации
+                </b-dropdown-item>
+                <b-dropdown-item :href="isAdmin ? `/admin/area-info/${id_org}` : '/area-info'">Сведения о колличесве
+                  мест и площади
+                </b-dropdown-item>
+                <b-dropdown-item :href="isAdmin ? `/admin/living-info/${id_org}` : '/living-info'">Сведения о
+                  проживающих
+                </b-dropdown-item>
+                <b-dropdown-item :href="isAdmin ? `/admin/living-info-inv/${id_org}` : '/living-info-inv'">Сведения о
+                  проживающих лицах <br> с ограниченными возможностями
+                </b-dropdown-item>
+                <b-dropdown-text>
 
-                <i>
-                  Данные о жилом объекте
-                </i>
-              </b-dropdown-text>
-              <b-dropdown-item :href="isAdmin ? `/admin/objects-info/${id_org}` : '/objects-info'">Сведения о жилом объекте</b-dropdown-item>
-              <b-dropdown-item :href="isAdmin ? `/admin/objects-area/${id_org}` : '/objects-area'">Сведения о площади, проживающих <br> и колличестве мест
-              </b-dropdown-item>
-              <b-dropdown-item :href="isAdmin ? `/admin/objects-money/${id_org}` : '/objects-money'">Сведения о поступлениях и расходах</b-dropdown-item>
-              <b-dropdown-item :href="isAdmin ? `/admin/objects-tariff/${id_org}` : '/objects-tariff'">Сведения о тарифах</b-dropdown-item>
-              <b-dropdown-text v-can:user>
-                <i>
-                  Документы
-                </i>
-              </b-dropdown-text>
-              <b-dropdown-item :href="isAdmin ? `/admin/documents/${id_org}` : '/documents'">{{ isAdmin ? 'Документы' : 'Загрузить документы' }}</b-dropdown-item>
+                  <i>
+                    Данные о жилом объекте
+                  </i>
+                </b-dropdown-text>
+                <b-dropdown-item :href="isAdmin ? `/admin/objects-info/${id_org}` : '/objects-info'">Сведения о жилом
+                  объекте
+                </b-dropdown-item>
+                <b-dropdown-item :href="isAdmin ? `/admin/objects-area/${id_org}` : '/objects-area'">Сведения о площади,
+                  проживающих <br> и колличестве мест
+                </b-dropdown-item>
+                <b-dropdown-item :href="isAdmin ? `/admin/objects-money/${id_org}` : '/objects-money'">Сведения о
+                  поступлениях и расходах
+                </b-dropdown-item>
+                <b-dropdown-item :href="isAdmin ? `/admin/objects-tariff/${id_org}` : '/objects-tariff'">Сведения о
+                  тарифах
+                </b-dropdown-item>
+                <b-dropdown-text v-can:user>
+                  <i>
+                    Документы
+                  </i>
+                </b-dropdown-text>
+                <b-dropdown-item :href="isAdmin ? `/admin/documents/${id_org}` : '/documents'">
+                  {{ isAdmin ? 'Документы' : 'Загрузить документы' }}
+                </b-dropdown-item>
 
-            </b-dropdown>
+              </b-dropdown>
 
-          </div>
-          <div class=" col m-auto"><span class="font-weight-bold">Количество объектов: {{ objCnt }}</span></div>
-          <div class=" col m-auto"><span class="font-weight-bold">Выгрузить в excel
+            </div>
+            <div class=" col m-auto"><span class="font-weight-bold">Количество объектов: {{ objCnt }}</span></div>
+            <div class=" col m-auto"><span class="font-weight-bold">Выгрузить в excel
             <i class="fas text-success fa-file-excel"></i>
           </span>
-          </div>
-          <div v-can:user v-if="saveButton" class=" col mt-2 mb-2">
+            </div>
+            <div v-can:user v-if="saveButton" class=" col mt-2 mb-2">
 
-            <transition
-                mode="out-in"
-                name="banner-apper">
-              <b-button pill :key="blockSave" class="font-weight-bold" size="sm" variant="outline-primary" block
-                        @click="click">{{ blockSave ? 'Редактировать' : 'Сохранить' }}
-              </b-button>
-            </transition>
+              <transition
+                  mode="out-in"
+                  name="banner-apper">
+                <b-button pill :key="blockSave" class="font-weight-bold" size="sm" variant="outline-primary" block
+                          @click="click">{{ blockSave ? 'Редактировать' : 'Сохранить' }}
+                </b-button>
+              </transition>
 
+            </div>
           </div>
         </div>
       </div>
+
+      <b-alert v-can:user v-if="!blockSave" show>
+        <div class="text-center">
+          "После внесения данных на странице не забудьте нажать кнопку "Сохранить"
+        </div>
+      </b-alert>
+
     </div>
 
-    <b-alert v-can:user v-if="!blockSave" show>
-      <div class="text-center">
-        "После внесения данных на странице не забудьте нажать кнопку "Сохранить"
-      </div>
-    </b-alert>
-
-  </div>
-
-  <div style="margin-top: 10px" v-can:user></div>
+    <div style="margin-top: 10px" v-can:user></div>
   </div>
 
 
@@ -91,7 +107,7 @@ export default {
   },
   props: {
     id_org: null,
-    isAdmin:false,
+    isAdmin: false,
     saveButton: {
       default: true,
     },
@@ -104,16 +120,16 @@ export default {
   },
   watch: {
     async id_org() {
-     // console.log(this.id_org)
+      // console.log(this.id_org)
       await this.getObjCnt();
     },
   },
   async mounted() {
-  await this.getObjCnt()
-    },
+    await this.getObjCnt()
+  },
   methods: {
     async getObjCnt() {
-     // console.log(this.id_org)
+      // console.log(this.id_org)
       if (this.id_org) {
         await Axios.get(`/api/cnt-objects/org/${this.id_org}`).then((res) => this.objCnt = res.data);
       }
@@ -140,6 +156,7 @@ export default {
   //height: 100%;
   transition: all .6s ease;
   z-index: 056576995769567965;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
 }
 
 .dropdown-menu {
@@ -183,7 +200,7 @@ export default {
       height: 0;
       border-style: solid;
       border-width: 0 5px 5px 5px;
-     // border-color: transparent transparent #fff transparent;
+      // border-color: transparent transparent #fff transparent;
       z-index: 99999999;
     }
   }
